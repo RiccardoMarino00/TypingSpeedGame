3e021ffd28f9f6cfc13b4309204bba1c2877735c
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-11-06 11:07:28 +0100

    Updated timer function and eventListener

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+                                                                                    // ELEMENTI
 
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random?minLength=300&maxLength=500';
 let output = document.querySelector('.output');
 let phrase = document.querySelector('.phrase');
 let input = document.querySelector('.user-input');
@@ -14,58 +16,14 @@ let tagletters = [];
 let lettere = [];
 let isCorrect = true;
 let previousLength = 0;
-
-function creatag() {
-    parole = phrase.textContent.split(' ');
-    parole.forEach(parola => {
-        const tagword = document.createElement('word');
-        for (let lettera of parola) {
-            const tagletter = document.createElement('letter');
-            tagletter.textContent = lettera;
-            tagletters.push(lettera);
-            lettere.push(lettera);
-            tagword.appendChild(tagletter);
-        }
-        phrase.innerHTML = '';
-        output.appendChild(tagword);
-
-    });
-
-}
-
-async function startTimer() {
-    if(playClick == false) {
-            score.textContent = 'score: 0'
-            input.value = '';
-            input.focus();
-            await getNextQuote();
-            console.log(output)
-            tagletters = [];
-            lettere = [];
-            score.textContent = 'Score: 0';
-            creatag();
-            let sec = 59;
-            playClick = true;
-            interval = setInterval(() => {
-                let formattedSec = sec.toString().padStart(2, '0');
-                time.innerHTML = '00:' + formattedSec;
-                sec --
-            }, 1000); 
-
-            setTimeout(function() {
-                clearInterval(interval);
-                playClick = false;
-                output.textContent = [];
-                phrase.textContent = 'Try Again';  
-                input.value = '';
-            }, 60000);
-        
-    }
-}
+let highestScore = 0;
 
 
+                                                                                    //FUNZIONI
+
+//riavvia timer
 function restartTimer() {
-    time.innerHTML = '01:00'
+    time.innerHTML = '60'
     clearInterval(interval);
     playClick = false;
     phrase.textContent = 'Play';
@@ -75,75 +33,220 @@ function restartTimer() {
 }
 
 
+//timer conto alla rovescia
+function countdown() {
+    let remain = +time.innerHTML - 1;
+    time.innerHTML = remain;
+    if (remain > 0) {
+        setTimeout(countdown, 1000)
+    }
+}
 
-play.addEventListener('click', startTimer );
 
-restart.addEventListener('click', restartTimer );
+//clicco play
+async function startTimer() {
+    await getNextQuote();
+    setTimeout(countdown, 1000)
+    input.focus()
+}
 
 
-// API per frasi
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random?minLength=300&maxLength=500';
-
+// chiamata api
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
     .then(response => response.json())
     .then(data => data.content)
 }
 
+
+
+//salvataggio frase
 async function getNextQuote() {
-    phrase.textContent = await getRandomQuote()
+    const quote = await getRandomQuote()
+    phrase.innerHTML = ''
     console.log(phrase)
+
+    quote.split('').forEach(char => {
+        const charSpan = document.createElement('span');
+        charSpan.innerText = char;
+        phrase.appendChild(charSpan)
+    })
+    input.value = null;
 }
 
 
 
+                                                                                    //EVENT LISTENER
 
 input.addEventListener('input', () => {
-    let inputArray = input.value.split(''); // Divide l'input dell'utente in un array di lettere
-    console.log(inputArray)
+    const span = phrase.querySelectorAll('span')
+    const inputValue = input.value.split('');
+    const lengthInput = input.value.length;
+    let currentScore = 0;
+ 
+    span.forEach((outputSpan, i) => {
+            const inputChar = inputValue[i]
 
-    const lengthInput = inputArray.length;
-    const length = Math.min(inputArray.length, lettere.length); //Math.min garantisce che non si vada mai oltre la lunghezza della frase stessa.
+                    if (inputChar === undefined) {
+                        outputSpan.classList.remove('green')
+                        outputSpan.classList.remove('red')
 
-    let currentText = score.textContent;
-    let currentScore  = parseInt(currentText.split(': ')[1]);
-               
-                let i = lengthInput - 1; //prende indice ultima lettera digitata
-                let j = lengthInput;
-                let letter = document.querySelectorAll('letter')
-                letter 
-                inputArray
-                console.log(letter[i])
-                console.log(inputArray)
-                if (lengthInput > previousLength) { // Verifica se l'input è più lungo rispetto al precedente (nuova lettera inserita)
-                    if (inputArray[i] === lettere[i]) {
-                        console.log('letter giusta', inputArray[i])
-                        console.log(letter[i])
-                        console.log(tagletters[i])
-                        letter[i].classList.add('green');
-                        letter[i].classList.remove('red');
-
+                    } else if (inputChar === outputSpan.innerText && lengthInput > previousLength) {
+                        outputSpan.classList.add('green')
+                        outputSpan.classList.remove('red')
                         currentScore += 1;
-                        console.log(currentScore);
                         score.textContent = `Score: ${currentScore}`;
-                        
-                    } else {
-                        console.log('letter sbagliata' , inputArray[i])
-                        letter[i].classList.add('red');
-                        letter[i].classList.remove('green');
+                     
 
-                    }
-                }  else {
-                    console.log('ha cancellato')
-                    if(letter[j].classList.contains('green')) {
-                        letter[j].classList.remove('green');
+
+                    } else if ( lengthInput === 0) {
+                        outputSpan.classList.remove('green')                    //FIXME: Non toglie primo punto fatto quando si cancella
+                        outputSpan.classList.add('red')                         //FIXME: Gli spazi li conta come punto fatto
+                        currentScore = 0;                                       //FIXME: togliere possibilità di ricliccare sulla phrase/play
+                        score.textContent = `Score: ${currentScore}`;           //FIXME: sitemare riavvia 
+                    }  else if (lengthInput < previousLength) {
                         currentScore -= 1;
                         score.textContent = `Score: ${currentScore}`;
-                    } else if (letter[j].classList.contains('red')) {
-                        letter[j].classList.remove('red');
-
+                    } else {
+                        outputSpan.classList.remove('green')
+                        outputSpan.classList.add('red')
                     }
 
-                }
-            previousLength = lengthInput;
+                    if (lengthInput === i) {
+                        span[i].classList.add('currentLetter')
+                        // outputSpan.scrollIntoView({ behavior: "auto", block: "center", inline: "center" });
+                    } else {
+                        span[i].classList.remove('currentLetter')
+
+                    }
+           
+            
+    })
+    // countPoints()
 })
+
+
+
+play.addEventListener('click', startTimer );
+
+restart.addEventListener('click', restartTimer );
+
+
+
+
+
+
+
+
+// function creatag() {
+//     parole = phrase.textContent.split(' ');
+//     parole.forEach(parola => {
+//         const tagword = document.createElement('word');
+//         for (let lettera of parola) {
+//             const tagletter = document.createElement('letter');
+//             tagletter.textContent = lettera;
+//             tagletters.push(lettera);
+//             lettere.push(lettera);
+//             tagword.appendChild(tagletter);
+//         }
+//         phrase.innerHTML = '';
+//         output.appendChild(tagword);
+
+//     });
+
+// }
+
+
+// async function startTimer() {
+//     if(playClick == false) {
+//             score.textContent = 'score: 0'
+//             input.value = '';
+//             input.focus();
+//             await getNextQuote();
+//             setTimeout(countdown, 1000)
+//             console.log(output)
+//             tagletters = [];
+//             lettere = [];
+//             score.textContent = 'Score: 0';
+//             // creatag();
+//             let sec = 59;
+//             playClick = true;
+//             // setTimeout(coutdown, 1000);
+
+//             interval = setInterval(() => {
+//                 let formattedSec = sec.toString().padStart(2, '0');
+//                 time.innerHTML = '00:' + formattedSec;
+//                 sec --
+//             }, 1000); 
+
+//             setTimeout(function() {
+//                 clearInterval(interval);
+//                 playClick = false;
+//                 output.textContent = [];
+//                 phrase.textContent = 'Try Again';  
+//                 input.value = '';
+//             }, 60000);
+        
+//     }
+// }
+
+
+// async function getNextQuote() {
+//     phrase.textContent = await getRandomQuote()
+//     console.log(phrase)
+// }
+
+
+
+
+
+
+// input.addEventListener('input', () => {
+//     let inputArray = input.value.split(''); // Divide l'input dell'utente in un array di lettere
+//     console.log(inputArray)
+
+//     const lengthInput = inputArray.length;
+//     const length = Math.min(inputArray.length, lettere.length); //Math.min garantisce che non si vada mai oltre la lunghezza della frase stessa.
+
+//     let currentText = score.textContent;
+//     let currentScore  = parseInt(currentText.split(': ')[1]);
+               
+//                 let i = lengthInput - 1; //prende indice ultima lettera digitata
+//                 let j = lengthInput;
+//                 let letter = document.querySelectorAll('letter')
+//                 letter 
+//                 inputArray
+//                 console.log(letter[i])
+//                 console.log(inputArray)
+//                 if (lengthInput > previousLength) { // Verifica se l'input è più lungo rispetto al precedente (nuova lettera inserita)
+//                     if (inputArray[i] === lettere[i]) {
+//                         console.log('letter giusta', inputArray[i])
+//                         console.log(letter[i])
+//                         console.log(tagletters[i])
+//                         letter[i].classList.add('green');
+//                         letter[i].classList.remove('red');
+
+//                         currentScore += 1;
+//                         console.log(currentScore);
+//                         score.textContent = `Score: ${currentScore}`;
+                        
+//                     } else {
+//                         console.log('letter sbagliata' , inputArray[i])
+//                         letter[i].classList.add('red');
+//                         letter[i].classList.remove('green');
+
+//                     }
+//                 }  else {
+//                     console.log('ha cancellato')
+//                     if(letter[j].classList.contains('green')) {
+//                         letter[j].classList.remove('green');
+//                         currentScore -= 1;
+//                         score.textContent = `Score: ${currentScore}`;
+//                     } else if (letter[j].classList.contains('red')) {
+//                         letter[j].classList.remove('red');
+
+//                     }
+
+//                 }
+//             previousLength = lengthInput;
+// })
